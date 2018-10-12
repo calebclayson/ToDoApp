44a79bf385709de61b2146cfd40a9e22232a29c1
Caleb <clayson.caleb@gmail.com>
2018-10-12 10:00:21 -0600

    got rid of mdl and reworking to the point I was before without it do to it roadblocking further interation from the user

--- js/main.js
+++ js/main.js
@@ -10,58 +10,20 @@ dialog.querySelector('.close').addEventListener('click', function () {
     dialog.close();
 });
 
-let itemIncrement = 1;
-let listIncrement = 2;
-
-function addList () {
-    let listName;
-    let tab = `
-        <a href="#fixed-tab-${listIncrement}" class="mdl-layout__tab">Test</a>
-    `;
-    let section = `
-        <section class="mdl-layout__tab-panel" id="fixed-tab-${listIncrement}">
-            <div class="page-content">
-                <h1>lol</h1>
-                <ul class="demo-list-control mdl-list" id="list-container">
-                </ul>
-                <button class="show-modal mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
-                    <i class="material-icons">add</i>
-                </button>
-                <dialog class="mdl-dialog">
-                    <div class="mdl-dialog__content">
-                        <p>
-                            Add List Item
-                        </p>
-                        <input type="text" id="newListItem">
-                    </div>
-                    <div class="mdl-dialog__actions mdl-dialog__actions--full-width">
-                        <button onclick="addItem()" type="button" class="mdl-button close">Done</button>
-                    </div>
-                </dialog>
-            </div>
-        </section>
-    `;
-    document.getElementById('tab-container').innerHTML += tab;
-    document.getElementById('main-content').innerHTML += section;
-    listIncrement++;
+let lists = {
+    currentList: {
+        name: 'Honey Do',
+        tasks: [
+            {
+                text: 'clean bathroom',
+                done: false
+            }
+        ]
+    }
 }
 
-function addItem() {
-    let whatToDo = document.getElementById('newListItem').value;
-    let content = `
-        <li class="mdl-list__item">
-            <span class="mdl-list__item-primary-content">
-                <i class="material-icons  mdl-list__item-icon">assignment</i>
-                ${whatToDo}
-            </span>
-            <span class="mdl-list__item-secondary-action">
-                <label for="chkbox${itemIncrement}" class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-                    <input type="checkbox" id="chkbox${itemIncrement}" class="mdl-checkbox__input">
-                </label> 
-            </span>
-        </li >
-        `;
-    document.getElementById('list-container').innerHTML += content;
-    itemIncrement++;
-    document.getElementById('newListItem').value = "";
+function displayCurrentList () {
+    document.getElementById('current-list-name').innerText = lists.currentList.name;
 }
+
+displayCurrentList();
